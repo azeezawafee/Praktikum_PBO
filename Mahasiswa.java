d8140fac2b63a950032cc2e5f756177b2431585f
Wafiq Azizah <azeezawafee@gmail.com>
2026-05-20 14:04:39 -0300

    Mahasiswa.java

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package tugas11;
-
-/**
- *
- * @author HELLO
- */
 public class Mahasiswa {
     private String nim, nama, alamat, makul;
     double nilaiAkhir;
@@ -19,10 +9,6 @@ public class Mahasiswa {
         this.makul = makul;
         this.nilaiAkhir = nilaiAkhir;
     }
-
-    /*Mahasiswa(String nim, String nama, String alamat, String makul, double nilaiAkhir) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }*/
     
     public String getnim(){
         return nim;
